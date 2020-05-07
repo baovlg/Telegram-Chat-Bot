390972f8da4adb013e5c46b793d644c41c5e24f8
gbao <vlgbao97@gmail.com>
2020-05-07 14:09:04 +0700

    Test new kind

---
+++ bot.js
@@ -0,0 +1,24 @@
+const token = process.env.TELEGRAM_TOKEN;
+
+const Bot = require('node-telegram-bot-api');
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+  console.log(OK)
+}
+
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
+
+bot.on('message', (msg) => {
+  const name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
+    // reply sent!
+  });
+});
+
+module.exports = bot;
